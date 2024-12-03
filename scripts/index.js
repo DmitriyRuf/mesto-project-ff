6e7b98d8a7e6657a798e5eb0452819bea2cd7707
Dmitriy Kurak <dmitriykurak@yandex.ru>
2024-12-03 22:15:56 +0300

    Добавление функции добавления и удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+//DOM узлы
+const content = document.querySelector(".content");
+const places = content.querySelector(".places");
+const placesList = places.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+function addCard(cardValues, deleteCard) {
+  //Элементы карточки
+  const placesItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const cardImage = placesItem.querySelector(".card__image");
+  const cardtitle = placesItem.querySelector(".card__title");
+  const cardDeleteButton = placesItem.querySelector(".card__delete-button");
+  //Заполнение картинки для карточки
+  cardImage.setAttribute("src", cardValues.link);
+  cardImage.setAttribute("alt", cardValues.name);
+  //Заполнение заголовка для карточки
+  cardtitle.textContent = cardValues.name;
+  //Определение функции для иконки удаления
+  cardDeleteButton.addEventListener("click", deleteCard);
+  //Возвращение заполненной карточки
+  return placesItem;
+}
 
-// @todo: Функция удаления карточки
+//Функция удаления карточки
+function deleteCard(event) {
+  const placesItem = event.target.closest(".places__item");
+  placesItem.remove();
+}
 
-// @todo: Вывести карточки на страницу
+//Вывести карточки на страницу
+initialCards.forEach(function (cardValues) {
+  const placesItem = addCard(cardValues, deleteCard);  
+  placesList.append(placesItem);
+});
