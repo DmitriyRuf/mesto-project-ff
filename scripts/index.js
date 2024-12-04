92a789b091d9c0e7f6ddf1ef3d90e8acaec7ff61
Dmitriy Kurak <dmitriykurak@yandex.ru>
2024-12-04 17:11:26 +0300

    Исправление замечаний по первому ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
-//Темплейт карточки
+/**Темплейт карточки*/
 const cardTemplate = document.querySelector("#card-template").content;
 
-//DOM узлы
+/**DOM узлы*/
 const content = document.querySelector(".content");
 const places = content.querySelector(".places");
-const placesList = places.querySelector(".places__list");
+const cardsContainer = places.querySelector(".places__list");
 
-//Функция создания карточки
-function addCard(cardValues, deleteCard) {
-  //Элементы карточки
-  const placesItem = cardTemplate.querySelector(".places__item").cloneNode(true);
-  const cardImage = placesItem.querySelector(".card__image");
-  const cardtitle = placesItem.querySelector(".card__title");
-  const cardDeleteButton = placesItem.querySelector(".card__delete-button");
-  //Заполнение картинки для карточки
-  cardImage.setAttribute("src", cardValues.link);
-  cardImage.setAttribute("alt", cardValues.name);
-  //Заполнение заголовка для карточки
-  cardtitle.textContent = cardValues.name;
-  //Определение функции для иконки удаления
+/**Функция создания карточки*/
+function createCard(cardData, deleteCard) {
+  /**Элементы карточки*/
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const cardImage = cardItem.querySelector(".card__image");
+  const cardtitle = cardItem.querySelector(".card__title");
+  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
+  /**Заполнение картинки для карточки*/
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  /**Заполнение заголовка для карточки*/
+  cardtitle.textContent = cardData.name;
+  /**Определение функции для иконки удаления*/
   cardDeleteButton.addEventListener("click", deleteCard);
-  //Возвращение заполненной карточки
-  return placesItem;
+  /**Возвращение заполненной карточки*/
+  return cardItem;
 }
 
-//Функция удаления карточки
+/**Функция удаления карточки*/
 function deleteCard(event) {
-  const placesItem = event.target.closest(".places__item");
-  placesItem.remove();
+  const cardItem = event.target.closest(".places__item"); 
+  cardItem.remove();
 }
 
-//Вывести карточки на страницу
-initialCards.forEach(function (cardValues) {
-  const placesItem = addCard(cardValues, deleteCard);  
-  placesList.append(placesItem);
+/**Вывести карточки на страницу*/
+initialCards.forEach(function (cardData) {
+  const cardItem = createCard(cardData, deleteCard);  
+  cardsContainer.append(cardItem);
 });
